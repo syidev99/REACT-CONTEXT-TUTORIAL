f4e73706271a485afa93ec95083876859c63d872
syidev99 <syidev99@gmail.com>
2022-10-10 04:01:16 +0900

    react-context-tutorial

--- src/contexts/color.js
+++ src/contexts/color.js
@@ -1,3 +1,25 @@
-import { createContext } from "react";
-const ColorContext = createContext({ color: "black" });
+import React, { createContext, useState } from "react";
+
+const ColorContext = createContext({
+    state: { color: "black", subcolor: "red" },
+    actions: {
+        setColor: () => {},
+        setSubcolor: () => {},
+    },
+});
+
+const ColorProvider = ({ children }) => {
+    const [color, setColor] = useState("black");
+    const [subcolor, setSubcolor] = useState("red");
+
+    const value = {
+        state: { color, subcolor },
+        actions: { setColor, setSubcolor },
+    };
+    return <ColorContext.provider value={value}>{children}</ColorContext.provider>;
+};
+
+const { Consumer: ColorConsumer } = ColorContext;
+export { ColorProvider, ColorConsumer };
+
 export default ColorContext;
